5a9c6eb25a1326c48dfc7237fa048493ad8eb5f7
Jeremy <75492743+MinhIT9@users.noreply.github.com>
2024-04-04 12:59:15 +0700

    Commit

--- obj/Debug/net8.0/Bina.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bina.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+633fa84bde62e308c56b00ca47d115b90613bd2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196edbf742e1a156381df769818ac591d3877ee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
