25afd060c0fea5727c58a45c348ce58d6712c43d
xquynh <xuanquynh9511@gmail.com>
2024-04-24 16:41:54 +0700

    ok

--- obj/Debug/net8.0/Bina.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bina.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e800c648a65860b248ed7ce9f2866abbb69a4e19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a02a70c50c7d9c28290a987675b251f77e0a51e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
