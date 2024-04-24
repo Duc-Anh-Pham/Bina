69acbf15bd2aef0fb4f670a7db92b214f7da5729
Jeremy <75492743+MinhIT9@users.noreply.github.com>
2024-04-24 11:59:22 +0700

    wait

--- obj/Debug/net8.0/Bina.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bina.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8e412454bd8953cd897725a05b36337b50cd15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f346e317560bb5816b9bd8f6bd433278410b8b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
