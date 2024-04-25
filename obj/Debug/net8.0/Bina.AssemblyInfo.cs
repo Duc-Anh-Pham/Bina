18a3978d0ded7b88200154138399c4251cdf0822
Jeremy <75492743+MinhIT9@users.noreply.github.com>
2024-04-25 11:22:22 +0700

    ok layout homepage user

--- obj/Debug/net8.0/Bina.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bina.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be01b286c66d98ca4f7b36fe02545d99f4b23b74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+566a65050d8f650aec169d1de4ffc91577806564")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
