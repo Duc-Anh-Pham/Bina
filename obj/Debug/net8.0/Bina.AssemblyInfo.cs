1a4202f267eb0e7644e585f2cf2138df90016f50
Tphuc14122003 <129703883+Tphuc14122003@users.noreply.github.com>
2024-04-25 22:38:49 +0700

    ok

--- obj/Debug/net8.0/Bina.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bina.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+780e4132ad190f75f071fb2828a408269329e4a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ac7dcc6d3757c6053b8bda6e5b8503b1e79ca5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
