c655788642310143bf5fe82c837853bec43a878e
anunicore <anunicore@gmail.com>
2024-04-30 02:07:42 +0700

    New test

--- Data/Ft1Context.cs
+++ Data/Ft1Context.cs
@@ -46,7 +46,7 @@ public partial class Ft1Context : DbContext
     {
         modelBuilder.Entity<Article>(entity =>
         {
-            entity.HasKey(e => e.ArticleId).HasName("PK__Articles__9C6270C8FCD99836");
+            entity.HasKey(e => e.ArticleId).HasName("PK__Articles__9C6270C836DDD33A");
 
             entity.Property(e => e.ArticleId).HasColumnName("ArticleID");
             entity.Property(e => e.ArticleName)
@@ -90,7 +90,7 @@ public partial class Ft1Context : DbContext
 
         modelBuilder.Entity<ArticleComment>(entity =>
         {
-            entity.HasKey(e => e.CommentId).HasName("PK__ArticleC__C3B4DFAA1AA1B992");
+            entity.HasKey(e => e.CommentId).HasName("PK__ArticleC__C3B4DFAA2D905124");
 
             entity.Property(e => e.CommentId)
                 .HasDefaultValueSql("(newsequentialid())")
@@ -111,7 +111,7 @@ public partial class Ft1Context : DbContext
 
         modelBuilder.Entity<ArticleLike>(entity =>
         {
-            entity.HasKey(e => e.ArticleLikeId).HasName("PK__ArticleL__54C4A1CEB2E4A367");
+            entity.HasKey(e => e.ArticleLikeId).HasName("PK__ArticleL__54C4A1CEEF6FFE37");
 
             entity.ToTable("ArticleLike");
 
@@ -132,7 +132,7 @@ public partial class Ft1Context : DbContext
 
         modelBuilder.Entity<ArticleStatus>(entity =>
         {
-            entity.HasKey(e => e.ArticleStatusId).HasName("PK__ArticleS__3F0E2D6B7BE90BA1");
+            entity.HasKey(e => e.ArticleStatusId).HasName("PK__ArticleS__3F0E2D6BF4E405A5");
 
             entity.ToTable("ArticleStatus");
 
@@ -144,7 +144,7 @@ public partial class Ft1Context : DbContext
 
         modelBuilder.Entity<ArticlesDeadline>(entity =>
         {
-            entity.HasKey(e => e.ArticlesDeadlineId).HasName("PK__Articles__253F2FDC18990B80");
+            entity.HasKey(e => e.ArticlesDeadlineId).HasName("PK__Articles__253F2FDC5461C7BA");
 
             entity.ToTable("ArticlesDeadline");
 
@@ -171,7 +171,7 @@ public partial class Ft1Context : DbContext
 
         modelBuilder.Entity<CommentFeedback>(entity =>
         {
-            entity.HasKey(e => e.CommentFeedbackId).HasName("PK__CommentF__9454C0BAD9FB8AD2");
+            entity.HasKey(e => e.CommentFeedbackId).HasName("PK__CommentF__9454C0BA264991D8");
 
             entity.ToTable("CommentFeedback");
 
@@ -194,7 +194,7 @@ public partial class Ft1Context : DbContext
 
         modelBuilder.Entity<Faculty>(entity =>
         {
-            entity.HasKey(e => e.FacultyId).HasName("PK__Faculty__306F636E6465867C");
+            entity.HasKey(e => e.FacultyId).HasName("PK__Faculty__306F636E3C49A208");
 
             entity.ToTable("Faculty");
 
@@ -208,7 +208,7 @@ public partial class Ft1Context : DbContext
 
         modelBuilder.Entity<HelpAndSupport>(entity =>
         {
-            entity.HasKey(e => e.HelpSupportId).HasName("PK__HelpAndS__65D53B0FE235F67D");
+            entity.HasKey(e => e.HelpSupportId).HasName("PK__HelpAndS__65D53B0FA4D54A32");
 
             entity.ToTable("HelpAndSupport");
 
@@ -228,7 +228,7 @@ public partial class Ft1Context : DbContext
 
         modelBuilder.Entity<Role>(entity =>
         {
-            entity.HasKey(e => e.RoleId).HasName("PK__Role__8AFACE3AB49576B6");
+            entity.HasKey(e => e.RoleId).HasName("PK__Role__8AFACE3AAAFF241E");
 
             entity.ToTable("Role");
 
@@ -240,7 +240,7 @@ public partial class Ft1Context : DbContext
 
         modelBuilder.Entity<TermsAndCondition>(entity =>
         {
-            entity.HasKey(e => e.TermsId).HasName("PK__TermsAnd__C05EBE00747C2314");
+            entity.HasKey(e => e.TermsId).HasName("PK__TermsAnd__C05EBE005A4A1616");
 
             entity.Property(e => e.TermsId)
                 .ValueGeneratedNever()
@@ -250,7 +250,7 @@ public partial class Ft1Context : DbContext
 
         modelBuilder.Entity<User>(entity =>
         {
-            entity.HasKey(e => e.UserId).HasName("PK__User__1788CCAC252C9C64");
+            entity.HasKey(e => e.UserId).HasName("PK__User__1788CCACB6F0C2C5");
 
             entity.ToTable("User");
 
