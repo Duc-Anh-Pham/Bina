406dadaa116fca66cabd4478d1d5107f2ab1fcd2
Jeremy <75492743+MinhIT9@users.noreply.github.com>
2024-05-02 12:27:07 +0700

    Fix article ok

--- obj/Debug/net8.0/Bina.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bina.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55697db2c5ac08d4471b52714ac74ec419775b53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13d46cf4a68b7f047b9bb1c4c2df37ae76bdf647")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
